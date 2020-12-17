dc9255ba4866954745cbfddc702e88a52c5db419
mssisodiya <mssisodiya@bestpeers.com>
2020-12-17 12:38:24 +0530

    used hook

--- src/timer.jsx
+++ src/timer.jsx
@@ -1,52 +1,40 @@
-import React, { Component } from "react";
+import React, { useRef, useState } from "react";
 
-class Timer extends Component {
-  state = {
-    isOff: true,
-    h: 9,
-    m: 59,
-    s: 58,
+function Timer() {
+  const [timer, setTimer] = useState(3600);
+  const increment = useRef(null);
+
+  const handleStart = () => {
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
   };
 
-  startTimer = () => {
-    this.interval = setInterval(this.start, 1000);
+  const handlePause = () => {
+    clearInterval(increment.current);
   };
 
-  start = () => {
-    this.setState({ s: this.state.s + 1 });
-    if (this.state.s >= 60) this.setState({ m: this.state.m + 1, s: 0 });
-    if (this.state.m >= 60) this.setState({ h: this.state.h + 1, m: 0 });
+  const formatTime = () => {
+    const seconds = `0${timer % 60}`.slice(-2);
+    const min = `${Math.floor(timer / 60)}`;
+    const minutes = `0${min % 60}`.slice(-2);
+    const hours = `0${Math.floor(timer / 3600)}`.slice(-2);
+
+    return `${hours} : ${minutes} : ${seconds}`;
   };
 
-  stop = () => {
-    clearInterval(this.interval);
-  };
-
-  format() {
-    let hours = this.state.h;
-    let seconds = this.state.s;
-    let minutes = this.state.m;
-
-    minutes = minutes < 1 ? "00" : minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 1 ? "00" : seconds < 10 ? `0${seconds}` : seconds;
-    hours = hours < 1 ? "00" : hours < 10 ? `0${hours}` : hours;
-    return `${hours}:${minutes}:${seconds}`;
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>HH : MM : SS</h2>
-        <h2>{this.format()}</h2>
-        <button className="btn btn-primary btn-lg" onClick={this.startTimer}>
-          Start
-        </button>
-        <button className="btn btn-danger btn-lg" onClick={this.stop}>
-          Stop
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>HH : MM : SS</h2>
+      <h2>{formatTime()}</h2>
+      <button className="btn btn-primary btn-lg" onClick={handleStart}>
+        Start
+      </button>
+      <button className="btn btn-danger btn-lg" onClick={handlePause}>
+        Stop
+      </button>
+    </div>
+  );
 }
 
 export default Timer;
